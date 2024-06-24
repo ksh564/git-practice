a3aa41e2de34991d41675b164283de233c5f1444
SeungHoon Kim <ksh564@hotmail.co.kr>
2024-06-25 00:46:51 +0900

    add - 트림 기능 개발 하였습니다.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("테스트1!");
+        System.out.println("기능 개발 하였습니다.");
     }
 }
