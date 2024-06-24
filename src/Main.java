a4342ef21a71b3d6b0cba1df7055bd04a4f86cca
SeungHoon Kim <ksh564@hotmail.co.kr>
2024-06-24 23:50:49 +0900

    테스트1에서 기입한 내용 + 2에서 기입한 내용

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello worlddsf!1");
+        System.out.println("테스트1에서 기입한 내용입니다.");
         System.out.println("테스트2에서 기입한 내용입니다.");
     }
 }
